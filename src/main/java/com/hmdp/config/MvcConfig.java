3e348d11205726e028fe3d290e4ec7c6bfcf4e60
HaoyuScut <904264204@qq.com>
2023-07-05 17:21:27 +0800

    黑马点评，用session实现登录校验

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName: {NAME}
+ * @Auther: why
+ * @Date: 2023/07/05 16 23
+ * @Version: v1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**"
+                );
+    }
+}
