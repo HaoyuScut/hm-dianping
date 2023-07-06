46c7931404e32573fd02e4f3891badf52b35b7c3
HaoyuScut <904264204@qq.com>
2023-07-06 23:01:25 +0800

    缓存击穿问题

---
+++ src/main/java/com/interview/cacheBreakdown.java
@@ -0,0 +1,17 @@
+package com.interview;
+
+/**
+ * @ClassName: {NAME}
+ * @Auther: why
+ * @Date: 2023/07/06 22 57
+ * @Version: v1.0
+ */
+public class cacheBreakdown {
+
+    /**
+     *缓存击穿问题也叫热点key问题。就是一个被高并发访问并且缓存重建业务
+     *较复杂的key突然失效了，无数的请求访问会在瞬间给数据库带来巨大的冲击
+     *
+     *
+     */
+}
