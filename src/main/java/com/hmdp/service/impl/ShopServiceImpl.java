548972ae2d2019ee528d9d3260ea7a070468552d
HaoyuScut <904264204@qq.com>
2023-07-06 22:42:24 +0800

    缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -15,23 +15,22 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
- * <p>
  * 服务实现类
- * </p>
- *
  * @author why
- * @Autowired 和 @Resource 都是用来实现依赖注入的注解（在 Spring/Spring Boot 项目中），但二者却有着 5 点不同：
- * <p>
+ * @since 2021-12-22
+ * @Autowired 和 @Resource 都是用来实现依赖注入的注解（在 Spring/Spring Boot 项目中），
+ * 但二者却有着 5 点不同：
  * 来源不同：@Autowired 来自 Spring 框架，而 @Resource 来自于（Java）JSR-250；
  * 依赖查找的顺序不同：@Autowired 先根据类型再根据名称查询，而 @Resource 先根据名称再根据类型查询；
  * 支持的参数不同：@Autowired 只支持设置 1 个参数，而 @Resource 支持设置 7 个参数；
  * 依赖注入的用法支持不同：@Autowired 既支持构造方法注入，又支持属性注入和 Setter 注入，而 @Resource 只支持属性注入和 Setter 注入；
  * 编译器 IDEA 的提示不同：当注入 Mapper 对象时，使用 @Autowired 注解编译器会提示错误，而使用 @Resource 注解则不会提示错误。
- * @since 2021-12-22
+ *
+ * 测试缓存穿透时显示服务端异常：Redis客户端输入：
+ * config set stop-writes-on-bgsave-error no
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
@@ -51,10 +50,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //判断命中的是否是空值
+        if(shopJson != null) {
+            // 返回一个错误信息
+            return Result.fail("店铺不存在!");
+        }
+
         //4.不存在，根据id查询数据库
         Shop shop = getById(id);
         //5.不存在，返回错误
         if(shop == null) {
+            //将空值写入Redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            //返回错误信息
             return Result.fail("店铺不存在!");
         }
         //6.存在，写入redis
