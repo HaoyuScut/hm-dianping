876f185ecbc5e7b91403905399b7b3b70c0e9e58
HaoyuScut <904264204@qq.com>
2023-07-10 14:53:25 +0800

    自己实现可用的分布式锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,7 +8,10 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +20,7 @@ import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -32,8 +35,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         //1.查询
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -53,17 +58,62 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足！");
 
         }
-        //5.扣减库存
+
+        Long userId = UserHolder.getUser().getId();
+
+        //创建锁对象
+        SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order:" + userId);
+
+        //获取锁
+        boolean isLock = lock.tryLock(1200);
+        //判断是否获取锁成功
+        if(!isLock) {
+            //获取锁失败，返回错误信息或重试
+            return Result.fail("不允许重复下单！");
+        }
+        try {
+            //获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        } catch (IllegalStateException e) {
+            throw new RuntimeException(e);
+        }finally {
+            lock.unLock();
+        }
+
+        /*synchronized(userId.toString().intern()) {
+            //获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }*/
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        //5.一人一单
+        Long userId = UserHolder.getUser().getId();
+
+
+        //5.1 查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        //5.2 判断是否存在
+        if (count > 0) {
+            //用户已经购买过
+            return Result.fail("用户已经购买过一次！");
+        }
+
+        //6.扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1") //set stock = stock - 1
                 .eq("voucher_id", voucherId)
 //                .eq("stock", voucher.getStock()) //CAS
                 .gt("stock", 0)
                 .update();
-        if(!success) {
+        if (!success) {
             //扣减失败
             return Result.fail("库存不足！");
         }
+
 
         //6.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
@@ -71,13 +121,13 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
         //6.2 用户id
-        Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
         //6.3 代金券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
 
-        //7.返回orderId
         return Result.ok(orderId);
+
+
     }
 }
---
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -0,0 +1,56 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.lang.UUID;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName: {NAME}
+ * @Auther: why
+ * @Date: 2023/07/10 14 28
+ * @Version: v1.0
+ * 需求:修改之前的分布式锁实现，满足:
+ * 1．在获取锁时存入线程标示（可以用UUID表示)
+ * 2．在释放锁时先获取锁中的线程标示，判断是否与当前线程标示一致
+ * 如果一致则释放锁
+ * 如果不一致则不释放锁
+ */
+public class SimpleRedisLock implements ILock{
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    private String name;//业务名称、锁的名称
+
+    private static final String KEY_PREFIX = "lock:";
+
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.name = name;
+    }
+
+    @Override
+    public boolean tryLock(long timeoutSec) {
+        //获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //获取锁
+        Boolean success = stringRedisTemplate.opsForValue()
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
+        return Boolean.TRUE.equals(success);
+    }
+
+    @Override
+    public void unLock() {
+        //获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //获取锁中的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(threadId.equals(id)){
+            //释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
+
+    }
+}
