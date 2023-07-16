e5e66c60717b67cf9aacacdfe53715130c1b3063
HaoyuScut <904264204@qq.com>
2023-07-16 20:59:19 +0800

    关注和取关功能实现

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -7,14 +7,18 @@ import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+import static com.hmdp.utils.RedisConstants.FOLLOW_KEY;
+
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -22,36 +26,47 @@ import javax.annotation.Resource;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 关注或取关
-     * @param followId
+     *
+     * @param followUserId
      * @param isFollow
      * @return
      */
     @Override
-    public Result follow(Long followId, Boolean isFollow) {
+    public Result follow(Long followUserId, Boolean isFollow) {
         //1.获取当前登录的用户
         UserDTO user = UserHolder.getUser();
-        if(user == null) {
+        if (user == null) {
             //用户未登录，无需查询是否点赞
             return Result.fail("用户未登录");
         }
         Long userId = UserHolder.getUser().getId();
+        String key = FOLLOW_KEY + userId;
         //2.判断是关注还是取关
         if (isFollow) {
             //3.关注，新增数据
             Follow follow = new Follow();
             follow.setUserId(userId);
-            follow.setFollowUserId(followId);
-            save(follow);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+            if(isSuccess) {
+                //把关注用户的id放入redis的集合中 sadd userId followUserId
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+            }
 
         } else {
             //4.取关，删除数据 delete from tb_follow where userId = ? and followId = ?
             LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(Follow::getUserId, userId).eq(Follow::getFollowUserId,followId);
-            remove(queryWrapper);
+            queryWrapper.eq(Follow::getUserId, userId).eq(Follow::getFollowUserId, followUserId);
+            boolean isSuccess = remove(queryWrapper);
+            if(isSuccess) {
+                //把关注用户的id从redis中移除
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+            }
         }
 
         return Result.ok();
@@ -62,14 +77,14 @@ public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> impleme
         //1.查询是否关注
         //1.获取当前登录的用户
         UserDTO user = UserHolder.getUser();
-        if(user == null) {
+        if (user == null) {
             //用户未登录，无需查询是否点赞
             return Result.fail("用户未登录");
         }
         //2.查询是否关注 select count(*) from tb_follow where
         Long userId = UserHolder.getUser().getId();
         LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Follow::getUserId, userId).eq(Follow::getFollowUserId,followUserId);
+        queryWrapper.eq(Follow::getUserId, userId).eq(Follow::getFollowUserId, followUserId);
         int count = count(queryWrapper);
         //3.判断
         return Result.ok(count > 0);
